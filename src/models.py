c51e1b4c3d523a554b3b2af21b03e19654fdb673
Fernando Santana <fernandosbcn@gmail.com>
2024-12-26 20:54:59 +0000

    La temida tuple

--- src/models.py
+++ src/models.py
@@ -37,7 +37,6 @@ class Planets(db.Model):
             "name": self.name,
         }
 
-
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
